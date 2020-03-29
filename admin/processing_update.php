4bf256983014bc98681d58f619f2d38128e4979b
msoderblom <soderblom.matilda@gmail.com>
2020-03-29 15:34:20 +0200

    Added update-file

---
+++ admin/processing_update.php
@@ -0,0 +1,9 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id_p = $_POST['id'];
+    $title_p = $_POST['title'];
+    $body_p = $_POST['body'];
+    $iframe_p = $_POST['iframe'];
+
+}
---
+++ admin/update.php
@@ -0,0 +1,60 @@
+<?php
+require_once '../db.php';
+require_once 'header.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && !isset($_POST['update'])) {
+
+    $id = htmlspecialchars($_POST['post_id']);
+
+    $sql = 'SELECT * FROM mecms_posts
+            WHERE id = :id';
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $title = htmlspecialchars($row['title']);
+        $body = strip_tags(htmlspecialchars_decode($row['body']));
+        $iframe = htmlspecialchars_decode($row['iframe']);
+        $img = $row['img'];
+    }
+
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['update'])) {
+    require_once 'processing_update.php';
+}
+
+?>
+<form action="#" method="POST" enctype="multipart/form-data">
+  <div class="form-group">
+    <label for="post_title">Title</label>
+    <input type="text" name="title" value="<?=$title?>" id="post_title" class="form-control" placeholder="Title"
+      aria-label="Post title">
+  </div>
+  <div class="form-group">
+    <label for="post_body">Text</label>
+    <textarea name="body" class="form-control" id="post_body" rows="3"><?=$body?></textarea>
+  </div>
+  <div class="form-group">
+    <label for="post_iframe">Embed map or video</label>
+    <textarea name="embedded_iframe" class="form-control" id="post_iframe" rows="3"><?=$iframe?></textarea>
+  </div>
+  <img src="../images/<?=$img?>">
+  <div class="form-group">
+    <label for="post_img">Select new image</label>
+    <input type="file" name="img_file" class="form-control-file" id="img_file" aria-describedby="fileHelp">
+    <small id="fileHelp" class="form-text text-muted">This is some placeholder block-level help text for the above
+      input. It's a bit lighter and easily wraps to a new line.</small>
+  </div>
+  <input type="hidden" name="update" value="true">
+  <input type="hidden" name="id" value="<?=$id?>">
+  <button type="submit" class="btn btn-primary">Update</button>
+
+</form>
+
+
+
+
+<?php
+require_once 'footer.php';
